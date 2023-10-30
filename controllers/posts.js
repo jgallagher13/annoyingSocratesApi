40bdc3917ccc4c4bbb8142fcef994b5723dfc103
jgallagher13 <jenngallagher13@gmail.com>
2023-10-30 08:46:22 -0600

    add populate on create

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,6 +3,7 @@ const Post = require('../models/post')
 async function create(req, res, next) {
     try {
         const post = await Post.create(req.body)
+        await post.populate('user')
         res.status(201).json(post)
     } catch (error) {
         console.log(error)
